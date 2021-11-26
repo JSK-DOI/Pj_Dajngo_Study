e7251951cde3c6c2c5434d64b90f7f9623e1a13c
JSK-DOI <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>
2021-11-26 19:57:50 +0900

    リストのフィルタ機能追加

--- KintaiKanri/views.py
+++ KintaiKanri/views.py
@@ -5,6 +5,7 @@ from .models import TKntiDtl
 from .models import MPjKnr
 from .forms import KintaiNyuryokuForm
 from django.views.generic import ListView
+from django.db.models import Q  
 
 
 def index(request):
@@ -16,5 +17,19 @@ class KintaiNyuryoku(CreateView):
     form_class = KintaiNyuryokuForm
     
 class PjKanriView(ListView):
-    model = MPjKnr
+    # model = MPjKnr
  #  template_name = ".html"
+    def get_queryset(self):
+        strFind_Key = self.request.GET.get('PJknr_list_Find')
+
+        if strFind_Key:
+            object_list = MPjKnr.objects.filter(
+                # Q(pj_no=strFind_Key) | Q(pj_nm=strFind_Key)
+                Q(pj_no__icontains=strFind_Key) | Q(pj_nm__icontains=strFind_Key)
+            )
+        else:
+            object_list = MPjKnr.objects.all()
+
+        # return super().get_queryset()
+        return object_list
+    
